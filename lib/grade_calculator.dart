6298cfd285c127612d23f441defa55d5a6afb2c5
Amin BADH <aminbadh@gmail.com>
2024-01-02 15:14:53 +0100

    Add Course Card

--- lib/grade_calculator.dart
+++ lib/grade_calculator.dart
@@ -5,21 +5,53 @@ class GradeCalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 12 * 2),
-        Opacity(
-          opacity: .7, // TODO - Make this part of the Theme
-          child: TextField(
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.displaySmall,
-            decoration: const InputDecoration(
-              hintText: 'Untitled',
-              border: InputBorder.none,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const SizedBox(height: 12 * 2),
+          Opacity(
+            opacity: .7, // TODO - Make this part of the Theme
+            child: TextField(
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.displaySmall,
+              decoration: const InputDecoration(
+                hintText: 'Untitled',
+                border: InputBorder.none,
+              ),
             ),
           ),
+          const SizedBox(height: 12 * 2),
+          const CourseCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseCard extends StatelessWidget {
+  const CourseCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Column(
+          children: [
+            TextField(
+              style: Theme.of(context).textTheme.titleMedium,
+              decoration: const InputDecoration(
+                isDense: true,
+                hintText: 'Course Title',
+                border: InputBorder.none,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
