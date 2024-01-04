622b7ec17d54e2448c8eeb023886bc6ceac27705
Amin BADH <aminbadh@gmail.com>
2024-01-04 15:55:33 +0100

    Update Course Card

--- lib/grade_calculator.dart
+++ lib/grade_calculator.dart
@@ -3,27 +3,38 @@ import 'package:flutter/material.dart';
 class GradeCalculator extends StatelessWidget {
   const GradeCalculator({super.key});
 
+  static const double sep = 12 * 2;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          const SizedBox(height: 12 * 2),
-          Opacity(
-            opacity: .7, // TODO - Make this part of the Theme
-            child: TextField(
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.displaySmall,
-              decoration: const InputDecoration(
-                hintText: 'Untitled',
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          const SizedBox(height: 12 * 2),
-          const CourseCard(),
+          SizedBox(height: sep),
+          DocTitle(),
+          SizedBox(height: sep),
+          CourseCard(),
         ],
+      ),
+    );
+  }
+}
+
+//TODO - Add opacity factor
+class DocTitle extends StatelessWidget {
+  const DocTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.displaySmall,
+      decoration: const InputDecoration(
+        hintText: 'Untitled',
+        border: InputBorder.none,
       ),
     );
   }
@@ -37,30 +48,37 @@ class CourseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      surfaceTintColor: Colors.white, //FIXME - hardcoded
+      elevation: 2,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
         child: Column(
           children: [
-            TextField(
-              style: Theme.of(context).textTheme.titleLarge,
-              decoration: const InputDecoration(
-                isDense: true,
-                hintText: 'Course Title',
-                border: InputBorder.none,
+            Row(children: [
+              Expanded(
+                child: TextField(
+                  style: Theme.of(context).textTheme.titleLarge,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    hintText: 'Course Title',
+                    border: InputBorder.none,
+                  ),
+                ),
               ),
-            ),
-            TextField(
-              style: Theme.of(context).textTheme.bodyMedium,
-              decoration: const InputDecoration(
-                isDense: true,
-                hintText: 'Course Code',
-                border: InputBorder.none,
+              Text(
+                '3',
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-            ),
+              Icon(
+                Icons.bolt,
+                color: Theme.of(context).colorScheme.tertiary,
+              ),
+            ]),
             const Divider(),
-            const Column(
-              children: [
-                Row(
+            const Column(children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
                   children: [
                     Expanded(
                       child: Text(
@@ -82,22 +100,20 @@ class CourseCard extends StatelessWidget {
                     ),
                   ],
                 ),
-              ],
-            ),
-            const Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.delete_outline),
-                  tooltip: 'Remove',
-                )
-              ],
-            ),
+              ),
+            ]),
           ],
         ),
       ),
     );
   }
 }
+
+/*TextField(
+  style: Theme.of(context).textTheme.bodyMedium,
+  decoration: const InputDecoration(
+    isDense: true,
+    hintText: 'Course Code',
+    border: InputBorder.none,
+  ),
+),*/
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100], //FIXME - hardcoded
       body: CustomScrollView(
         slivers: [
           SliverFillRemaining(
